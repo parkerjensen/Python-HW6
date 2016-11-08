334b156aa50d3f48e020d4d034f775d767602148
Parker Jensen <parker.jensen@aggiemail.usu.edu>
2016-11-08 13:34:25 -0700

    added almost complete regex checking

--- parker_jensen_hw6.py
+++ parker_jensen_hw6.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env pythons
 import sys
 from urllib.request import urlopen
+import re
 
 def getErrorLog(url):
     """
@@ -10,27 +11,43 @@ def getErrorLog(url):
     Returns:
         List of errors
     """
+    errorLog = dict()
     message = urlopen(url)
     log = message.read().decode()
     splitLog = log.splitlines()
     for line in splitLog:
-        splitLine = line.split(']')
-        if splitLine[1] == ' [error':
-            print(splitLine[3])
+        try:
+            splitLine = line.split(']')
+            if splitLine[1] == ' [error':
+                line = splitLine[3]
+                #print(line)
+                file = re.findall('/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+                #file = re.match(r"(/.)+?(\.[a-z]+)",line)
+                #print(file.group(0))
+                try:
+                    if(file[0] in errorLog):
+                        errorLog[file[0]] += 1
+                    else:
+                        errorLog[file[0]] = 1
+                except:
+                    pass
+        except:
+            pass
 
-        
-    #splitLog = splitLog.split('[')
-    #splitLog = splitLog.split(']')
-    #print(splitLog)
-    #print(log)
-
-
+    errors = sorted(errorLog, key=errorLog.get, reverse=True)
+    occures = sorted(errorLog.values(), reverse=True)
+    i = 0
+    print("*** Top 25 page errors ***")
+    while i < 25:
+        print("Count: ", occures[i], "  Page: ", errors[i])
+        i += 1
+    #print(errorLog)
 
 def main():
     """
     Test Function
     """
-    getErrorLog('http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test')
+    getErrorLog('http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full')
     return
 
 if __name__=="__main__":
